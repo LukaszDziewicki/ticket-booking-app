06b7cd17fecf509e832683a7a648e86240378575
Lukasz <dziewickilukasz@gmail.com>
2021-07-05 23:56:46 +0200

    edit BigDecimal in enum class

--- src/main/java/com/example/ticketbookingapp/TicketTypes.java
+++ src/main/java/com/example/ticketbookingapp/TicketTypes.java
@@ -3,9 +3,9 @@ package com.example.ticketbookingapp;
 import java.math.BigDecimal;
 
 public enum TicketTypes {
-    ADULT(new BigDecimal(25)),
-    STUDENT(new BigDecimal(18)),
-    CHILD(new BigDecimal(12.50));
+    ADULT(BigDecimal.valueOf(25)),
+    STUDENT(BigDecimal.valueOf(18)),
+    CHILD(BigDecimal.valueOf(12.50));
 
     private BigDecimal price;
 
